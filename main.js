5eeb3be4c3ec9d97325cfe16a8dfaadb45ab2ad0
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-01-23 09:06:26 -0500

    Mejora el código para la validación del correo y evita que el texto de display sea seleccionado

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
 (() => {
-	const forms = document.querySelectorAll(".needs-validation");
-	const result = document.getElementById("result");
-    for(const form of forms) {
-        form.addEventListener("submit", event => {
-            event.preventDefault();
-            event.stopPropagation();
-            if (!form.checkValidity()) {
-                form.querySelectorAll(":invalid")[0].focus();
-            } else {
-                const formData = new FormData(form);
-                const object = Object.fromEntries(formData);
-                const json = JSON.stringify(object);
-                result.textContent = 'Enviando...';
+	const form = document.querySelector("form");
+	const resultado = document.getElementById("resultado");
+    form.addEventListener("submit", event => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (!form.checkValidity()) {
+            form.querySelectorAll(":invalid")[0].focus();
+        } else {
+            const formData = new FormData(form);
+            const object = Object.fromEntries(formData);
+            const json = JSON.stringify(object);
+            resultado.textContent = 'Enviando...';
 
-                fetch("https://api.web3forms.com/submit", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json",
-                    },
-                    body: json,
+            fetch("https://api.web3forms.com/submit", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: json,
+            })
+                .then(async (response) => {
+                    const json = await response.json();
+                    if (response.status === 200) {
+                        resultado.textContent = json.message;
+                    } else {
+                        console.log(response);
+                        resultado.textContent = json.message;
+                    }
                 })
-                    .then(async (response) => {
-                        const json = await response.json();
-                        if (response.status === 200) {
-                            result.textContent = json.message;
-                        } else {
-                            console.log(response);
-                            result.textContent = json.message;
-                        }
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        result.textContent = 'Algo salió mal, inténtalo más tarde o contáctame directamente.';
-                    })
-                    .then(() => {
-                        form.reset();
-                        form.classList.remove("was-validated");
-                        result.textContent = 'Gracias por escribirme, pronto me estaré poniendo en contacto :)';
-                        setTimeout(() => {
-                            result.classList.add("hidden")
-                        }, 4000);
-                    });
-            }
-            form.classList.add("was-validated");
-        },
-            false
-        );
-    }
+                .catch((error) => {
+                    console.log(error);
+                    resultado.textContent = 'Algo salió mal, inténtalo más tarde o contáctame directamente.';
+                })
+                .then(() => {
+                    form.reset();
+                    form.classList.remove("was-validated");
+                    resultado.textContent = 'Gracias por escribirme, pronto me estaré poniendo en contacto :)';
+                    setTimeout(() => resultado.classList.add("hidden"), 4000);
+                });
+        }
+        form.classList.add("was-validated");
+    },
+        false
+    );
 })();
