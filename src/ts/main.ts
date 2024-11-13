7f5bd7dad9be3e1acb77b4a2f4414f4138534ed5
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-11-13 12:53:03 -0500

    Incorpora el cambio de idioma

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,36 @@
 import '../css/style.css';
 
+//set lang
+const initializeLang = () => {
+    let lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
+    lang = lang === 'es' ? 'es' : 'en';
+    localStorage.setItem('lang', lang);
+    document.documentElement.setAttribute('lang', lang);
+    return lang;
+};
+initializeLang();
+
+const toggleLang = () => {
+    let lang = localStorage.getItem('lang');
+    menuLang.textContent = `(${lang?.toUpperCase()})`;
+    lang = lang === 'es' ? 'en' : 'es';
+    localStorage.setItem('lang', lang);
+    document.documentElement.setAttribute('lang', lang);
+};
+
 // menu
-const menuLang = document.querySelector<HTMLButtonElement>('.menu__lang');
-const menuLinks = document.querySelector<HTMLUListElement>('.menu__links');
-const menuBtn = document.querySelector<HTMLButtonElement>('.menu__show');
+const menuLang = document.querySelector<HTMLButtonElement>('.menu__lang')!;
+const menuBtn = document.querySelector<HTMLButtonElement>('.menu__show')!;
+const menuLinks = document.querySelector<HTMLUListElement>('.menu__links')!;
 
-console.log(window.getComputedStyle(menuBtn).getPropertyValue('display'));
-
-menuBtn?.addEventListener('pointerdown', () => {
-    menuLinks?.classList.toggle('visible');
+menuLang.addEventListener('pointerdown', toggleLang);
+menuBtn.addEventListener('pointerdown', () => {
+    menuLinks.classList.toggle('visible');
     menuBtn.classList.toggle('view');
 });
-
-menuLinks?.querySelectorAll('a').forEach((btn) => {
+menuLinks.querySelectorAll('a').forEach((btn) => {
     btn.addEventListener('pointerdown', () => {
         menuLinks.classList.remove('visible');
-        menuBtn?.classList.toggle('view');
+        menuBtn.classList.toggle('view');
     });
 });
