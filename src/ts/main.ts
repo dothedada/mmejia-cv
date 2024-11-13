9178e6a79a5d67ebcb80b76d65d067ff95e2055d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-11-13 14:54:13 -0500

    Oculta el menu de forma automatica cuando se hace scroll

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -41,11 +41,33 @@ const toggleLang = (): void => {
 const initializeLang = () => setLang(getLang());
 initializeLang();
 
+const blockPageMove = (): void => {
+    const bloqued = document.body.style.overflow === 'hidden';
+    document.body.style.overflow = bloqued ? 'unset' : 'hidden';
+};
+
 // menu
+let userPosition = window.scrollY;
+window.addEventListener('scroll', () => {
+    const menu = document.querySelector<HTMLMenuElement>('.menu')!;
+    if (window.scrollY < 61) return;
+
+    window.requestAnimationFrame(() => {
+        if (window.scrollY < userPosition) {
+            menu.classList.remove('hide');
+        } else {
+            menu.classList.add('hide');
+        }
+
+        userPosition = window.scrollY;
+    });
+});
+
 menuLang.addEventListener('pointerdown', toggleLang);
 menuBtn.addEventListener('pointerdown', () => {
     menuLinks.classList.toggle('visible');
     menuBtn.classList.toggle('view');
+    blockPageMove();
 });
 menuLinks.querySelectorAll('a').forEach((btn) => {
     btn.addEventListener('pointerdown', () => {
