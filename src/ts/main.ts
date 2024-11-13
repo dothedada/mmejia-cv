e2e26ed540db13d2ec432995ee702e11f53e71f3
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-11-13 10:39:43 -0500

    Termina de ajustar el comportamiento del  menu en movil

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,20 @@
 import '../css/style.css';
-import { setupCounter } from './counter.ts';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+// menu
+const menuLang = document.querySelector<HTMLButtonElement>('.menu__lang');
+const menuLinks = document.querySelector<HTMLUListElement>('.menu__links');
+const menuBtn = document.querySelector<HTMLButtonElement>('.menu__show');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+console.log(window.getComputedStyle(menuBtn).getPropertyValue('display'));
+
+menuBtn?.addEventListener('pointerdown', () => {
+    menuLinks?.classList.toggle('visible');
+    menuBtn.classList.toggle('view');
+});
+
+menuLinks?.querySelectorAll('a').forEach((btn) => {
+    btn.addEventListener('pointerdown', () => {
+        menuLinks.classList.remove('visible');
+        menuBtn?.classList.toggle('view');
+    });
+});
