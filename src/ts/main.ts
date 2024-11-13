71792a5f317b2dc75d7330fec2470a21b0dbc4cc
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-11-13 13:36:53 -0500

    Limpia funcion para cambio de lenguaje

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,28 +1,47 @@
 import '../css/style.css';
+import txtSR from './ui-sr_txt';
 
-//set lang
-const initializeLang = () => {
-    let lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
-    lang = lang === 'es' ? 'es' : 'en';
-    localStorage.setItem('lang', lang);
-    document.documentElement.setAttribute('lang', lang);
-    return lang;
-};
-initializeLang();
+type Lang = 'es' | 'en';
 
-const toggleLang = () => {
-    let lang = localStorage.getItem('lang');
-    menuLang.textContent = `(${lang?.toUpperCase()})`;
-    lang = lang === 'es' ? 'en' : 'es';
-    localStorage.setItem('lang', lang);
-    document.documentElement.setAttribute('lang', lang);
-};
-
-// menu
 const menuLang = document.querySelector<HTMLButtonElement>('.menu__lang')!;
 const menuBtn = document.querySelector<HTMLButtonElement>('.menu__show')!;
 const menuLinks = document.querySelector<HTMLUListElement>('.menu__links')!;
 
+// lang
+const getLang = (): Lang => {
+    let lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
+    return lang === 'es' ? 'es' : 'en';
+};
+
+const setLangButtonContent = (lang: Lang): void => {
+    const srSpan = document.createElement('span');
+    const txtSpan = document.createElement('span');
+    menuLang.textContent = '';
+
+    srSpan.textContent = txtSR[lang].langBtn;
+    srSpan.classList.add('sr-only');
+    txtSpan.textContent = `(${lang === 'es' ? 'En' : 'Es'})`;
+    txtSpan.ariaHidden = 'true';
+
+    menuLang.append(srSpan, txtSpan);
+};
+
+const setLang = (lang: Lang): void => {
+    setLangButtonContent(lang);
+    localStorage.setItem('lang', lang);
+    document.documentElement.setAttribute('lang', lang);
+};
+
+const toggleLang = (): void => {
+    const currentLang = getLang();
+    const newLang: Lang = currentLang === 'es' ? 'en' : 'es';
+    setLang(newLang);
+};
+
+const initializeLang = () => setLang(getLang());
+initializeLang();
+
+// menu
 menuLang.addEventListener('pointerdown', toggleLang);
 menuBtn.addEventListener('pointerdown', () => {
     menuLinks.classList.toggle('visible');
